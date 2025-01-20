f44906e4583f7f3a2948e67e9a81f88020e7baaf
oMarquess <olufemimarquess@gmail.com>
2025-01-20 13:49:27 +0000

    rfn set to 107

--- facefusion/api/constants.py
+++ facefusion/api/constants.py
@@ -3,5 +3,5 @@ TARGET_VIDEO = "../target_video/papaya-global.mp4"
 OUTPUT_DIR = "output"
 UPLOAD_DIR = "uploads"
 REFERENCE_FACE_POSITION =  0
-REFERENCE_FRAME_NUMBER = 0
+REFERENCE_FRAME_NUMBER = 107
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
