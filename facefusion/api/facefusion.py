145d3f6c6c9f1ed5075ababeadb3fdf4216be638
awal99 <awalsulemana3@gmail.com>
2025-01-30 15:54:07 +0000

    hot fix

--- facefusion/api/facefusion.py
+++ facefusion/api/facefusion.py
@@ -137,7 +137,7 @@ async def process_face_fusion(
             "--output-video-quality", "95",
             "--face-detector-score", "0.3",
            	"--execution-device-id", "0",  # Set device ID (default 0)
-			# "--execution-providers", "cuda",
+			"--execution-providers", "cuda",
     		"--execution-thread-count", "32",  # Maximum thread count
     		"--execution-queue-count", "5",
 
@@ -204,7 +204,7 @@ async def process_face_fusion(
             "--output-video-quality", "95",
             "--face-detector-score", "0.3",
             "--execution-device-id", "0",  # Set device ID (default 0)
-			# "--execution-providers", "cuda",
+			"--execution-providers", "cuda",
             "--execution-thread-count", "32",  # Maximum thread count
             "--execution-queue-count", "5"
         ]
